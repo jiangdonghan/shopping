c28ca6ef40dc9ecdd5878d28708720c9187bdac2
donghanjiang <2641635339@qq.com>
2019-12-23 02:05:49 +0000

    fix order model relationship

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,7 @@ class OrdersController < ApplicationController
         @order = Order.find(params[:id])
         @product_lists = @order.product_lists
     end
+    
   def create
     @order = Order.new(order_params)
     @order.user = current_user
@@ -15,7 +16,7 @@ class OrdersController < ApplicationController
             product_list = ProductList.new
             product_list.order = @order
             product_list.product_name = cart_item.product.title
-            product_list.product_price = cart_item.product.product.price
+            product_list.product_price = cart_item.product.price
             product_list.quantity = cart_item.quantity
             product_list.save
         end
